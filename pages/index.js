4ac371f909ead66117b3dd038c87caa31c5a09cc
Isaac Lopez <isaaclopezprog@gmail.com>
2022-03-30 13:45:46 -0400

    1 avance, creacion de header

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+import Slider from '../components/Inicio/Slider';
+import Headers from '../layout/Headers';
+import Sidebar from '../layout/Sidebar'
+
+export default function index() {
+  return (
+    <>
+      {/* <div className="bg-slate-600">index</div> */}
+      <Headers>
+        <Slider />
+
+      </Headers>
+
+      {/* <Sidebar></Sidebar> */}
+    </>
+  )
+}
