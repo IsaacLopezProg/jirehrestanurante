56e09beac4a312aa4c2120162155e83223e2a697
Isaac Lopez <isaaclopezprog@gmail.com>
2022-05-27 09:02:55 -0400

    20 avance, creacion de seo e indexado

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+export default {
+    openGraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://www.jirehtropical.com/',
+        site_name: 'Jireh Tropical Restaurant',
+        description: 'A family business dedicated to serving our community. Covering food and nutritional needs with our range of services and a wide variety of healthy meals.',
+
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Headers from '../layout/Headers';
 import Inicio from '../components/Inicio/Index';
 import Footer from '../layout/Footer';
+import { NextSeo } from 'next-seo';
 
 export default function index() {
 
@@ -19,6 +20,11 @@ export default function index() {
 
   return (
     <>
+      <NextSeo
+        title="Jireh Tropical Restaurant"
+        description="A family business dedicated to serving our community. Covering food and nutritional needs with our range of services and a wide variety of healthy meals."
+        canonical="https://www.jirehtropical.com/"
+      />
       <Headers >
         {/* <Inicio idioma={idioma} /> */}
         <Inicio />
